56814839eff942304d00cadd04cf3c3885f25ec3
shernandezz <santihernandezzapata@gmail.com>
2020-02-19 12:16:56 -0500

    hola mundo

--- Clases/Clase 1/holamundo.py
+++ Clases/Clase 1/holamundo.py
@@ -2,6 +2,8 @@
 nombre = "MHF S.A.A"
 tiempo = 50
 dinero = 3664433223.99
+empleados = 55
 print("La variable nombre es igual a: " + nombre + " y es tipo: " + str(type(nombre)))
 print("La variable tiempo es igual a: " + str(tiempo) + " y es tipo: " + str(type(tiempo)))
 print("La variable dinero es igual a: " + str(dinero) + " y es tipo: " + str(type(dinero)))
+print("La variable empleados es igual a: " + str(empleados) + " y es tipo: " + str(type(empleados)))
