1b02b6744a09784cfde5a089232751e711e41b9f
shernandezz <santihernandezzapata@gmail.com>
2020-02-19 12:12:08 -0500

    hola mundo

---
+++ Clases/Clase 1/holamundo.py
@@ -0,0 +1,7 @@
+#codigo primera clase
+nombre = "MHF S.A.A"
+tiempo = 50
+dinero = 3664433223.99
+print("La variable nombre es igual a: " + nombre + " y es tipo: " + str(type(nombre)))
+print("La variable tiempo es igual a: " + str(edad) + " y es tipo: " + str(type(edad)))
+print("La variable dinero es igual a: " + str(dinero) + " y es tipo: " + str(type(dinero)))
