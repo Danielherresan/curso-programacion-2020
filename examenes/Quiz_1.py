fe8abb0e0e72048a30d25642861998d26d7b2a46
shernandezz <santihernandezzapata@gmail.com>
2020-02-28 12:52:10 -0500

    quiz 1

--- examenes/Quiz_1.py
+++ examenes/Quiz_1.py
@@ -1,10 +1,6 @@
 # MENSAJES
 MSJ_BVN = "Bienvenido al programa que te juzga y te clasifica por tu lugar de origen y ademas te dice lo que ya deberias saber."
 MSJ_EST = "En este momento te encuentras es un estado de {},"
-MSJ_SALUD = "En este momento te necuentras es un estado saludable,"
-MSJ_HIPO = "En este momento te necuentras es un estado de hipotermia,"
-MSJ_ALERTA = "En este momento te necuentras es un estado de alerta,"
-MSJ_PELIGRO = "En este momento te necuentras es un estado de peligro,"
 DESP_OBSV = "porfavor dirigete al centro de monitoreo lo mas antes posible."
 DESP_NORMAL = "que tengas un buen dia."
 DESP_ALERTA = "se recomienda que estes alerta y te realices examenes de salud."
@@ -14,7 +10,7 @@ PRG_TEMP = "Ingresa tu temperatura actual: "
 PAISES_BAN = ["china", "italia", "iran"]
 obsv = "observacion"
 hipo = "hipotermia"
-salud = "saludable"
+salud = "buena salud"
 alerta = "alerta"
 peligro = "peligro"
 
